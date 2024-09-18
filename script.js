bf63d6e2822bea2692d1d5042c56009ffb684de0
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2024-09-19 02:49:44 +0530

    changes in the file home page

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
